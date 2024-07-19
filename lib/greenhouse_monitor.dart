15385aa0606421e10770d6cca50eb2b76b63dd2b
tiiakeskinarkaus <tiia.keskinarkaus@centria.fi>
2024-07-19 09:36:58 +0300

    Tiian eka

--- lib/greenhouse_monitor.dart
+++ lib/greenhouse_monitor.dart
@@ -3,52 +3,40 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kasvihuonesovellus/greenhouse_viewmodel.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: GreenhouseMonitor(),
     );
   }
 }
 
 class GreenhouseMonitor extends ConsumerWidget {
+  const GreenhouseMonitor({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final greenhouseData = ref.watch(greenhouseViewModelProvider);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Greenhouse Monitor')),
+      appBar: AppBar(title: const Text('Greenhouse Monitor')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Temperature: ${greenhouseData.temperature}°C',
-                style: TextStyle(fontSize: 24)),
-            SizedBox(height: 20),
+                style: const TextStyle(fontSize: 24)),
+            const SizedBox(height: 20),
             Text('Humidity: ${greenhouseData.humidity}%',
-                style: TextStyle(fontSize: 24)),
+                style: const TextStyle(fontSize: 24)),
           ],
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              ref
-                  .read(greenhouseViewModelProvider.notifier)
-                  .updateTemperature(25.0);
-              ref
-                  .read(greenhouseViewModelProvider.notifier)
-                  .updateHumidity(60.0);
-            },
-            child: Icon(Icons.update),
-          ),
-        ],
       ),
     );
   }
--- lib/views/greenhouse_monitor.dart
+++ lib/views/greenhouse_monitor.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kasvihuonesovellus/greenhouse_viewmodel.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GreenhouseMonitor extends ConsumerWidget {
   @override
@@ -8,32 +9,50 @@ class GreenhouseMonitor extends ConsumerWidget {
     final greenhouseData = ref.watch(greenhouseViewModelProvider);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Greenhouse Monitor')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Temperature: ${greenhouseData.temperature}°C',
-                style: TextStyle(fontSize: 24)),
-            SizedBox(height: 20),
-            Text('Humidity: ${greenhouseData.humidity}%',
-                style: TextStyle(fontSize: 24)),
-          ],
-        ),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.white.withOpacity(0.7),
+        elevation: 0,
+        centerTitle: true,
+        title: Text("Kasvihuone", style: GoogleFonts.pacifico(fontSize: 50)),
+        toolbarHeight: 120,
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
+      body: Stack(
         children: [
-          FloatingActionButton(
-            onPressed: () {
-              ref
-                  .read(greenhouseViewModelProvider.notifier)
-                  .updateTemperature(25.0);
-              ref
-                  .read(greenhouseViewModelProvider.notifier)
-                  .updateHumidity(60.0);
-            },
-            child: Icon(Icons.update),
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/background.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Lämpötila: ${greenhouseData.temperature}°C',
+                    style: TextStyle(fontSize: 24)),
+                SizedBox(height: 20),
+                Text('Kosteus: ${greenhouseData.humidity}%',
+                    style: TextStyle(fontSize: 24)),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 16,
+            bottom: 150,
+            child: FloatingActionButton(
+              onPressed: () {
+                ref
+                    .read(greenhouseViewModelProvider.notifier)
+                    .updateTemperature(25.0);
+                ref
+                    .read(greenhouseViewModelProvider.notifier)
+                    .updateHumidity(60.0);
+              },
+              child: const Icon(Icons.update),
+            ),
           ),
         ],
       ),
